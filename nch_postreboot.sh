ac27ade6a967baba72960157965325697e59d6ac
coriollis-nico <nixillomax@live.com.mx>
2023-01-08 16:41:24 -0600

    Removed reduntant pacman config.

--- nch_postreboot.sh
+++ nch_postreboot.sh
@@ -29,8 +29,6 @@ systemctl enable firewalld.service &&
 systemctl start firewalld.service &&
 read -p 'Done [Enter to continue]'
 
-read -p '[Enter to configure pacman]'
-rnano /etc/pacman.conf
 read -p '[Enter to activate reflector (default config)]'
 systemctl enable reflector.service &&
 read -p 'Done [Enter to continue]'
