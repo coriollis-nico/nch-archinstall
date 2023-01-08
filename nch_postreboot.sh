ff29e802a27f00ca30e37957ceac10e56ac075d4
coriollis-nico <nixillomax@live.com.mx>
2023-01-08 03:53:24 -0600

    First testing version

--- nch_postreboot.sh
+++ nch_postreboot.sh
@@ -19,3 +19,25 @@ passwd coriollis
 chown $NEWUSER -R /home/$NEWUSER
 read -p 'Done [Enter to continue]'
 
+echo '[Enter to activate firewalld]'
+echo '(You will have to configure it afterwards)'
+systemctl enable firewalld.service
+systemctl start firewalld.service
+read -p 'Done [Enter to continue]'
+
+read -p '[Enter to configure pacman]'
+rnano /etc/pacman.conf
+read -p '[Enter to activate reflector (default config)]'
+systemctl enable reflector.service
+read -p 'Done [Enter to continue]'
+
+read -p '[Enter to start/enable thermald]'
+systemctl start thermald.service
+systemctl enable thermald.service
+read -p '[Enter to config. & enable cpupower]'
+rnano /etc/default/cpupower
+systemctl enable cpupower.service
+read -p 'Done (check the CPU frec. scaling wiki for specific configs. for DEs) [Enter to continue]'
+
+echo 'You now can run the encryption script'
+echo 'Also consider USBGuard and FIX LATER'
