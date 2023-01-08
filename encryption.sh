d324964e6419f0d0b7139cee7203e7fabdde0f42
coriollis-nico <nixillomax@live.com.mx>
2023-01-08 14:42:51 -0600

    Update encryption.sh

--- encryption.sh
+++ encryption.sh
@@ -1,13 +1,14 @@
 echo 'ENCRYPTION SCRIPT'
 echo '--Make sure to create the partition BEFORE running this (make sure it is encryption ready)'
+read -p '[Enter to start]'
 read -p 'Partition (no /dev/): ' PARTITION
 read -p 'User: ' SETUP_USER
-# Setuo & test
-cryptsetup luksFormat /dev/$PARTITION
-cryptsetup open /dev/$PARTITION $NAME
-mkfs.ext4 /dev/mapper/home-$SETUP_USER
-cryptsetup open device home-$SETUP_USER
-mount -t ext4 /dev/mapper/home-$SETUP_USER /mnt/home/$SETUP_USER
-umount /mnt/home/$SETUP_USER
-cryptsetup close home-$SETUP_USER
+# Setup & test
+cryptsetup luksFormat /dev/$PARTITION &&
+cryptsetup open /dev/$PARTITION $NAME &&
+mkfs.ext4 /dev/mapper/home-$SETUP_USER &&
+cryptsetup open device home-$SETUP_USER &&
+mount -t ext4 /dev/mapper/home-$SETUP_USER /mnt/home/$SETUP_USER &&
+umount /mnt/home/$SETUP_USER &&
+cryptsetup close home-$SETUP_USER &&
 # Auto-mounting
